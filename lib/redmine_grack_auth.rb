fd3b7159e497b70ea7abe4f20123e806f7c05ac7
posativ <info@posativ.org>
2011-10-19 17:05:45 +0200

    add more sophisticated auth, see #1 and #2
    Now, grack allows `git clone ...` from public repositories (allowing
    'git-upload-pack' with POST) and requests the user's redmine memberships
    on non-public projects.

--- lib/redmine_grack_auth.rb
+++ lib/redmine_grack_auth.rb
@@ -8,43 +8,49 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 class RedmineGrackAuth < Rack::Auth::Basic
 
-  def valid?(auth)
-    url = $grackConfig[:redmine]
-    return false if !url
+  def valid?(auth, identifier, url)
 
-    creds = *auth.credentials
-    user, pass = creds[0, 2]
-
-    identifier = get_project()
-    return false if !identifier
+    user, pass = *auth.credentials[0, 2]
     permission = (@req.request_method == "POST" && Regexp.new("(.*?)/git-receive-pack$").match(@req.path_info) ? 'rw' : 'r')
 
-    user = auth.username
-    password = auth.credentials[1]
     begin
-      open("#{url}/projects/#{identifier}.xml",
-           :http_basic_authentication => [user, password]) {}
+      r = /<membership><project name="lilith" id=/
+      uri = "#{url}/users/current.xml?include=memberships"
+      return false unless r.match(open(uri, :http_basic_authentication => [user, pass]).read())
     rescue OpenURI::HTTPError
       return false
     end
-
     return true
   end
 
   def call(env)
     @env = env  
     @req = Rack::Request.new(env)
-
-
-    return unauthorized if(not defined?($grackConfig))
-
+    
+    identifier = get_project()
+    url = $grackConfig[:redmine]
     auth = Rack::Auth::Basic::Request.new(env)
-    return unauthorized unless auth.provided?
-    return bad_request unless auth.basic?
-    return unauthorized unless valid?(auth)
-
-    env['REMOTE_USER'] = auth.username
-    return @app.call(env)
+    
+    return unauthorized if(not defined?($grackConfig))
+    return unauthorized if !identifier or !url
+    return bad_request if !$grackConfig[:redmine]
+    
+    if auth.provided?
+      return bad_request unless auth.basic?
+      return unauthorized unless valid?(auth, identifier, url)
+      env['REMOTE_USER'] = auth.username
+      return @app.call(env)
+    elsif @req.post? and @req.path_info.end_with?('git-receive-pack')
+      return unauthorized
+    else
+      begin
+        open("#{url}/projects/#{identifier}.xml")
+      rescue OpenURI::HTTPError => err
+        return bad_request unless err.io.status[0] == '401'
+        return unauthorized
+      end
+      return @app.call(env)
+    end
   end
 
   def get_project
